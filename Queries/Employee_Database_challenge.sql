eb42ed310b372e4cdcc614b10d2491cd341d1487
nypasha1928 <71052359+nypasha1928@users.noreply.github.com>
2020-11-09 16:40:23 -0500

    Pewlett-Hackard-Analysis

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,4 @@
--- Deliverable 1 (step 1 to 7)
+-- Deliverable 1 (step 1 to 7)  The Number of Retiring Employees by Title.
 SELECT e.emp_no,
 e.first_name, 
 e.last_name,
@@ -6,7 +6,7 @@ titles.title,
 titles.from_date,
 titles.to_date 
 
-INTO retirement_titles
+--INTO retirement_titles
 FROM employees as e
 INNER JOIN titles
 ON e.emp_no = titles.emp_no
@@ -28,8 +28,27 @@ ORDER BY emp_no, title DESC;
 -- Deliverable 1 ( step 14 to 21)
 
 SELECT COUNT(ut.emp_no), ut.title
-INTO retiring_titles
+--INTO retiring_titles
 FROM unique_titles as ut
 
 GROUP BY ut.title
 ORDER BY ut.count DESC;
+
+---------------------------------------------
+-- Deliverable 2: The Employees Eligible for the Mentorship Program.
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name, 
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date, 
+titles.title
+--INTO mentorship_eligibilty
+FROM employees as e
+  INNER JOIN dept_emp as de
+  INNER JOIN titles
+ON (e.emp_no = de.emp_no)
+WHERE de.to_date = (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no DESC;
+
